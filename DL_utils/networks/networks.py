ecedae8496956cec6c5b08c78cfa35b4a2cceeff
Manuel Diez Silva <madisi1998@gmail.com>
2020-02-23 17:58:43 +0100

    Added CNN1D

--- DL_utils/networks/networks.py
+++ DL_utils/networks/networks.py
@@ -1,7 +1,7 @@
-from .core import ModularNetwork
+from .core import Network
 
 
-class FCNN(ModularNetwork):
+class FCNN(Network):
     def __init__(self, params):
         super().__init__(params)
 
@@ -15,7 +15,25 @@ class FCNN(ModularNetwork):
         self.add_layer('Dense', self.output_shape)
 
 
-class CNN2D(ModularNetwork):
+class CNN1D(Network):
+    def __init__(self, params):
+        super().__init__(params)
+
+        self.architecture = params['architecture']
+        self.archNN = params['archNN']
+        self.padding = params['padding']
+
+        self.build_model()
+        self.compile()
+
+    def build_model(self):
+        self.add_layer('Conv1D', self.architecture)
+        self.add_layer('Flatten')
+        self.add_layer('Dense', self.archNN)
+        self.add_layer('Dense', self.output_shape)
+
+
+class CNN2D(Network):
     def __init__(self, params):
         super().__init__(params)
 
